4350be4e1b2b27c101cbb8b17481f1a9dfd19681
Luca Gambetta <l.gambetta@skylable.com>
2015-05-29 11:22:03 +0200

    Integrated Zeroclipboard 2.2.0
    Removed the old and obsolete zclip.
    This should fix the black button showed when Flash plugin is not installed.

--- public/layouts/skylable/js/file_operations.js
+++ public/layouts/skylable/js/file_operations.js
@@ -678,16 +678,45 @@ if (!FileOperations) {
                                         }
                                     ]);
                                     dlg.html(xhr.responseText);
-                                    $('#copytoclipboarddialogbtn').zclip({
-                                        path:'/layouts/skylable/js/ZeroClipboard.swf',
-                                        copy:$('.sharelink a').text(),
-                                        afterCopy:function() {
-                                            $( '#copytoclipboarddialogbtn' ).button( "option", "label", Skylable_Lang['shareCopiedToClipboard'] );
-                                            window.setTimeout(function(){
-                                                $( '#copytoclipboarddialogbtn' ).button( "option", "label", Skylable_Lang['shareCopyToClipboard'] );
-                                            }, 3000);
-                                        }
+                                    // Integrate zeroclipboard
+                                    ZeroClipboard.config({
+                                        swfPath: zeroclipboard_swf
                                     });
+
+                                    // Fix for IE: https://github.com/zeroclipboard/zeroclipboard/blob/master/docs/instructions.md#ie-freezes-when-clicking-a-zeroclipboard-clipped-element-within-a-jquery-ui-modal-dialog
+                                    if (/MSIE|Trident/.test(window.navigator.userAgent)) {
+                                        (function($) {
+                                            var zcClass = '.' + ZeroClipboard.config('containerClass');
+                                            $.widget( 'ui.dialog', $.ui.dialog, {
+                                                _allowInteraction: function( event ) {
+                                                    return this._super(event) || $( event.target ).closest( zcClass ).length;
+                                                }
+                                            } );
+                                        })(window.jQuery);
+                                    }
+                                    
+                                    var the_copy_to_clip_button = document.getElementById('copytoclipboarddialogbtn');
+                                    var clip = new ZeroClipboard( the_copy_to_clip_button );
+                                    clip.on('ready', function(event){
+                                        clip.on("copy", function(e){
+                                            var clipboard = e.clipboardData;
+                                            clipboard.setData("text/plain", $('.sharelink a').text() );
+                                        });
+                                        clip.on("aftercopy", function(e){
+
+                                            $( the_copy_to_clip_button ).button( "option", "label", Skylable_Lang['shareCopiedToClipboard'] );
+                                            window.setTimeout(function(){
+                                                $( the_copy_to_clip_button ).button( "option", "label", Skylable_Lang['shareCopyToClipboard'] );
+                                            }, 3000);
+                                            
+                                        });
+                                    });
+                                    
+                                    clip.on('error', function(event){
+                                        $( the_copy_to_clip_button).button("disable");
+                                        ZeroClipboard.destroy();
+                                    });
+
                                 },
                                 error : function(xhr, status) {
                                     if (!FileOperations.expiredUser(dlg, xhr)) {
