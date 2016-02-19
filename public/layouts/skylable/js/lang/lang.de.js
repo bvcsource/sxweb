1b0919a4396fb6f1f4631a216a5b1849a02d7a1a
Michał Pawluk <mpawluk@skylable.com>
2016-02-19 10:29:15 +0100

    Add "Notify me" string. Update German translations.

--- public/layouts/skylable/js/lang/lang.de.js
+++ public/layouts/skylable/js/lang/lang.de.js
@@ -57,13 +57,13 @@ if (!Skylable_Lang) {
         deleteNoFiles: "Keine Dateien ausgewählt",
 
         // --------- Share window
-        shareTitle : "Datei teilen",
-        shareDirTitle : "Share directory",
-        shareMsg : "Bist Du sicher, dass Du die Datei teilen möchtest?",
+        shareTitle : "Datei freigeben",
+        shareDirTitle : "Verzeichnis freigeben",
+        shareMsg : "Bist Du sicher, dass Du die Datei freigeben möchtest?",
         shareCopyToClipboard: "In Zwischenablage kopieren",
         shareCopiedToClipboard: "In Zwischenablage kopiert!",
         shareFile : 'Datei: ',
-	shareBtn : 'Share',
+		shareBtn : 'Freigeben',
 
         // -------- Preview
         previewErrorTitle : 'Vorschaufehler!',
@@ -114,6 +114,5 @@ if (!Skylable_Lang) {
         managevolumePrivRead : "Lesen",
         managevolumePrivWrite : "Schreiben",
         managevolumePrivManager : "Verwalten"
-        
     }
 }
